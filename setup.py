3e756d1ff079dc31b16ab7d14932b6c5b6752736
ByebyDoggy <byebyedoggy@outlook.com>
2025-11-25 11:58:40 +0800

    setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
+with open(r"C:\Users\26717\PycharmProjects\ArkmClient\requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
@@ -31,7 +31,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "your-command=your_package.main:main",
+            "arkm-client=arkm.main:main",
         ],
     },
     include_package_data=True,
